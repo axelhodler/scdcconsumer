efb0d7686b9dc7d120b958d223e061be40c6f8c4
Axel Hodler <axel@hodler.co>
2018-05-07 21:42:15 +0200

    init from start.spring.io

---
+++ src/main/java/co/hodler/scdcconsumer/ScdcconsumerApplication.java
@@ -0,0 +1,12 @@
+package co.hodler.scdcconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdcconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdcconsumerApplication.class, args);
+	}
+}
